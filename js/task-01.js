db2ee5787f7f61df5c984d54f7ebfdf096b46858
kirhal <kirillgaleckiy@gmail.com>
2023-02-03 22:39:55 +0200

    1 js change

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,9 @@
-const numberOfCategories = document.querySelectorAll(".item");
-console.log("Number of categories: ", numberOfCategories.length);
+const categories = document.querySelectorAll(".item");
+console.log("Number of categories: ", categories.length);
 
-console.log("Category: ", numberOfCategories[0].firstElementChild.textContent);
-console.log(
-  "Elements: ",
-  numberOfCategories[0].lastElementChild.children.length
-);
-console.log("Category: ", numberOfCategories[1].firstElementChild.textContent);
-console.log(
-  "Elements: ",
-  numberOfCategories[1].lastElementChild.children.length
-);
-console.log("Category: ", numberOfCategories[2].firstElementChild.textContent);
-console.log(
-  "Elements: ",
-  numberOfCategories[2].lastElementChild.children.length
-);
+console.log("Category: ", categories[0].firstElementChild.textContent);
+console.log("Elements: ", categories[0].lastElementChild.children.length);
+console.log("Category: ", categories[1].firstElementChild.textContent);
+console.log("Elements: ", categories[1].lastElementChild.children.length);
+console.log("Category: ", categories[2].firstElementChild.textContent);
+console.log("Elements: ", categories[2].lastElementChild.children.length);
