b5e3a567d485de3d252a68747e67e2f96719360b
kirhal <kirillgaleckiy@gmail.com>
2023-02-03 23:29:37 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,7 @@
 const categories = document.querySelectorAll(".item");
 console.log("Number of categories: ", categories.length);
 
-console.log("Category: ", categories[0].firstElementChild.textContent);
-console.log("Elements: ", categories[0].lastElementChild.children.length);
-console.log("Category: ", categories[1].firstElementChild.textContent);
-console.log("Elements: ", categories[1].lastElementChild.children.length);
-console.log("Category: ", categories[2].firstElementChild.textContent);
-console.log("Elements: ", categories[2].lastElementChild.children.length);
+for (const category of categories) {
+  console.log("Category: ", category.firstElementChild.textContent);
+  console.log("Elements: ", category.lastElementChild.children.length);
+}
