9f14d31a158e624be1c3728ca69b76649a96ac1f
kirhal <kirillgaleckiy@gmail.com>
2023-02-04 21:46:58 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -3,10 +3,6 @@ const decrementBtn = document.querySelector("button[data-action=decrement]");
 const incrementBtn = document.querySelector("button[data-action=increment]");
 const value = document.querySelector("#value");
 
-// const decreaseCounterValue = event =
-
-// const increaseCounterValue;
-
 decrementBtn.addEventListener("click", (Event) => {
   counterValue -= 1;
   value.textContent = counterValue;
