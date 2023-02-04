5089caad02c4601c55fd7fc951f61630ad4cde8f
kirhal <kirillgaleckiy@gmail.com>
2023-02-04 11:29:12 +0200

    2-nd updated

--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,12 @@ const ingredients = [
   "Condiments",
 ];
 const list = document.querySelector("#ingredients");
-const listArray = [];
 
-ingredients.forEach(function (ingredient) {
+const listArray = ingredients.map((ingredient) => {
   const listItem = document.createElement("li");
   listItem.textContent = ingredient;
   listItem.classList.add("item");
-  listArray.push(listItem);
+  return listItem;
 });
 
 list.append(...listArray);
