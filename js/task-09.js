d9520b74c53b37920dfc1231fd6166f41bba640e
kirhal <kirillgaleckiy@gmail.com>
2023-02-05 17:37:46 +0200

    9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const buttonEl = document.querySelector(".change-color");
+const currentColorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+buttonEl.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  bodyEl.style.background = getRandomHexColor();
+  currentColorEl.textContent = getRandomHexColor();
+}
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
